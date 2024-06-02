6b58b926789d41eedd0366bb3c020192aac295c5
Luis Villalon <luisvillalon@hotmail.es>
2024-06-02 19:16:16 +0200

    ejercicio lista y envoltorio

---
+++ Tema2/src/io/keepcoding/javadesdecero/javabasico/TestClaseEnvoltorio.java
@@ -0,0 +1,13 @@
+package io.keepcoding.javadesdecero.javabasico;
+
+public class TestClaseEnvoltorio {
+
+	public static void main(String[] args) {
+		Integer a= Integer.valueOf(5);
+		
+		System.out.println("Print numero desde su calse envoltorio " + a);
+		System.out.println("Print numero desde su calse envoltorio como float" + a.floatValue());
+
+	}
+
+}
--- Tema2/src/io/keepcoding/javadesdecero/javabasico/TestSwitch.java
+++ Tema2/src/io/keepcoding/javadesdecero/javabasico/TestSwitch.java
@@ -3,8 +3,32 @@ package io.keepcoding.javadesdecero.javabasico;
 public class TestSwitch {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String option="a";
+		String res ="";
+		
+		/*
+		if (option=="a") {
+			res="Go to the cinema";
+		} else if (option=="b") {
+			res="Go to the restaurant";
+		}else {
+			res="stay at home";
+		}
+		*/
+		
+		switch(option) {
+			case "a":
+				res="Go to cinema";
+				break;
+			case "b":
+				res="Go to the restaurant";
+				break;
+			default:
+				res="Stay at home";
+				break;
+		}
 
+		System.out.println(res);
 	}
 
 }
